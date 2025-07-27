cbc47fb9e4651d468866d840615bdbb7a3fe6368
mohamednousad <nousadnousad021@gmail.com>
2025-07-27 21:42:35 +0530

    folders changed

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const config = Object.freeze({
     port: process.env.PORT || 3000,
-    databaseURI: process.env.MONGODB_URI || "mongodb://localhost:27017/pos-db",
+    databaseURI: process.env.MONGODB_URI || "mongodb+srv://mrnoukhan7377:MyDB123@mydb.eymfa.mongodb.net/restaurant",
     nodeEnv : process.env.NODE_ENV || "development",
     accessTokenSecret: process.env.JWT_SECRET,
     razorpayKeyId: process.env.RAZORPAY_KEY_ID,
