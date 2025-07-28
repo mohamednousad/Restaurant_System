d24c0399567d4edf6ac5a865169d8a46887ea3af
mohamednousad <nousadnousad021@gmail.com>
2025-07-29 03:32:48 +0530

    configuration done

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ connectDB();
 // Middlewares
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173',"https://phyroze-restaurant.com"]
+    origin: ['http://localhost:5173',"https://phyroze-restaurant.vercel.app"]
 }))
 app.use(express.json()); // parse incoming request in json format
 app.use(cookieParser())
