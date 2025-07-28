43f17c9df54b83382a237dee5203988a00960611
mohamednousad <nousadnousad021@gmail.com>
2025-07-29 03:23:56 +0530

    domain changed

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ connectDB();
 // Middlewares
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173',"https://restaurant.com"]
+    origin: ['http://localhost:5173',"https://phyroze-restaurant.com"]
 }))
 app.use(express.json()); // parse incoming request in json format
 app.use(cookieParser())
